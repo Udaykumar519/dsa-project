64929fa285912f2211eae17dda16af5cee7045d9
udaykumarpolu <udaykumar.polu@gmail.com>
2026-02-06 10:38:36 +0530

    Added Tree DS | creation of tree and inorder traversal of tree

---
+++ src/Tree.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Tree{
+    static Scanner sc = null;
+
+    public static void main(String[] args) {
+        sc = new Scanner(System.in);
+        Node root = createTree();
+        Node.inorder(root);
+    }
+
+    static Node createTree(){
+        Node root = null;
+
+        System.out.print("Enter data: ");
+        int data = sc.nextInt();
+
+        if (data == -1) return null;
+
+        root = new Node(data);
+
+        System.out.println("Enter left for: " + data);
+        root.left = createTree();
+
+        System.out.println("Enter right for:" + data);
+        root.right = createTree();
+
+        return root;
+    }
+}
+
+class Node{
+    Node left, right;
+    int data;
+
+    public Node(int data){
+        this.data = data;
+    }
+
+    static void inorder(Node root) {
+        if (root == null) return;
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+}
